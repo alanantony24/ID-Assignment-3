c80d98d7130e3e437f5a32cfd9063ee7c4a1d8a0
alanantony24 <s10207327@connect.np.edu.sg>
2021-02-13 23:24:48 +0800

    made sidenav for main interface

--- js/main.js
+++ js/main.js
@@ -36,21 +36,6 @@ function logOutUser(logOutContent){
 function redirectToHome(){
     window.location.replace("index.html");
 }
-var navtab = document.getElementById("menuicon");
-var navmenu = document.getElementById("navmenu");
-navmenu.style.left = "-250px"
-navtab.onclick = function(){
-    if(navmenu.style.left == "-250px"){
-        navmenu.style.left = "0";
-    }
-    else{
-        navmenu.style.left = "-250px"
-    }
-}
-
-
-
-
 /*===================================SIDE NAVBAR AJAX FUNCTIONS====================================================*/
 
 var colorArray = 
@@ -109,6 +94,48 @@ function getAllProjects(colorArray){
         }
         var projectList = $('section#projects');
         projectList.html(content);
-    })
-        
+    })  
 };
+//Side Anvigation and Banner
+const showMenu = (toggleId, navbarId, bodyId)=>{
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId);
+    if(toggle && navbar){
+        toggle.addEventListener('click', ()=>{
+            navbar.classList.toggle('expander')
+            bodypadding.classList.toggle('body-pd')
+        })
+    }
+}
+showMenu('nav-toggle', 'navbar', 'body-pd')
+const linkcolor = document.querySelectorAll('.nav__link');
+function colorLink(){
+    linkcolor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+linkcolor.forEach(l => l.addEventListener('click', colorLink))
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+//Log OUt
+var logOutContent =  `
+<div class ="row justify-content-center">
+<div class = "col-xs-10 col-md-4">
+    <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_0fwl68.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player></div>
+</div>
+<div class ="row justify-content-center">
+<div class = "col-xs-10 col-md-4">
+    Please wait while we log you out . . .
+</div>
+</div>`; //loading animation content
+function logOutUser(logOutContent){
+  let $mainPage =$('body#body-pd').children().first(); 
+  $mainPage.remove();
+  $('body').prepend(logOutContent);
+  setTimeout(redirectToHome,2500);
+}
+function redirectToHome(){
+    window.location.replace("index.html");
+}
